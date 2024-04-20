a1334848bef55b810112834c129951e63a3be9b9
Dylan Garza <DAG.Gamer999@gmail.com>
2024-04-20 11:14:39 -0500

    Basic linear acceleration on one ship component.

--- Space Game Multiplayer Project/main.cpp
+++ Space Game Multiplayer Project/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <list>
 #include <raylib.h>
 #include <raymath.h>
@@ -30,45 +31,75 @@
 * ships explode when out of health
 */
 
+//used to align value changes to be done every second, non-frame-dependent
+float perSecond(float value) {
+	return value * GetFrameTime();                
+}
+
 float angleBetween(float x1, float y1, float x2, float y2) {
 	return atan2f(y2 - y1, x2 - x1);
 }
 
 class Component {
 public:
-	Component() {
-
+	Component(float tVel) {
+		hitbox = { 0.0f, 0.0f, 32.0f, 32.0f };
+		maxVelocity = tVel;
 	}
 
 	void control() {
-		angle = angleBetween(hitbox.x + hitbox.width / 2.0f, hitbox.y + hitbox.height / 2.0f, GetMouseX(), GetMouseY());
-
+		//control linear acceleration
+		acceleration = 0;
 		if (IsKeyDown(KeyboardKey::KEY_W)) {
-			hitbox.y -= 3;
-		}
-		if (IsKeyDown(KeyboardKey::KEY_A)) {
-			hitbox.x -= 3;
+			acceleration += 16;
 		}
 		if (IsKeyDown(KeyboardKey::KEY_S)) {
-			hitbox.y += 3;
+			acceleration -= 16;
 		}
+
+		//change and clamp velocity, (v = v0 + at)
+		velocity += perSecond(acceleration);
+		velocity = fminf(velocity, maxVelocity);
+		velocity = fmaxf(velocity, -maxVelocity);
+
+		//move
+		hitbox.x += perSecond(velocity) * cosf(angle);
+		hitbox.y += perSecond(velocity) * sinf(angle);
+
+		if (IsKeyDown(KeyboardKey::KEY_A)) {
+			angle -= perSecond(PI); //turn left
+		}
+		
 		if (IsKeyDown(KeyboardKey::KEY_D)) {
-			hitbox.x += 3;
+			angle += perSecond(PI); //turn right
 		}
 	}
 
 	void draw() {
-		Rectangle display = { hitbox.x + hitbox.width / 2.0f, hitbox.y + hitbox.height / 2.0f, hitbox.width, hitbox.height };
-		Vector2 center = { hitbox.width / 2.0f, hitbox.height / 2.0f };
+		Rectangle display = { hitbox.x + hitbox.width / 2.0f, hitbox.y + hitbox.height / 2.0f, hitbox.width, hitbox.height }; //display oriented inside hitbox
+		Vector2 center = { hitbox.width / 2.0f, hitbox.height / 2.0f }; //offset from origin of hitbox
 		DrawRectanglePro(display, center, RAD2DEG * angle, WHITE);
 
+		//debug displays
 		DrawRectangleLinesEx(hitbox, 2.0f, RED);
 		DrawCircle(hitbox.x, hitbox.y, 2.0f, RED);
+		DrawLine(display.x, display.y, display.x + 30 * cosf(angle), display.y + 30 * sinf(angle), RED);
 	}
+
+	void drawHUD(int x, int y) {
+		DrawText(TextFormat("a: %3.1f px/s^2", acceleration), 10 + x, 10, 10 + y, WHITE);
+		DrawText(TextFormat("v: %4.1f / %4.1f px/s", velocity, maxVelocity), 10 + x, 30 + y, 10, WHITE);
+		DrawText(TextFormat("x: %.1f, y: %.1f", hitbox.x, hitbox.y), 10 + x, 50 + y, 10, WHITE);
+		DrawText(TextFormat("angle: %.2f", angle), 10 + x, 70 + y, 10, WHITE);
+	}
+
 
 private:
 	Rectangle hitbox{};
-	float angle;
+	float angle{};
+	float velocity{};
+	float maxVelocity{}; //arbitrary
+	float acceleration{};
 };
 
 int main() {
@@ -76,25 +107,27 @@ int main() {
 		fprintf(stderr, "uh oh! enet didn't initialize!\n");
 		return EXIT_FAILURE;
 	}
-	atexit(enet_deinitialize);*/
+	atexit(enet_deinitialize);
+	std::cout << "ENET init successful.\n";*/
 
 	InitWindow(800, 800, "Space Game Multiplayer Project");
 	SetTargetFPS(60);
 
-	Component c;
+	Component c(480.0f);
 
 	while (!WindowShouldClose()) {
 		c.control();
 
 		BeginDrawing();
 		ClearBackground(BLACK);
-
+		
 		c.draw();
+		c.drawHUD(0, 0);
 
 		EndDrawing();
 	}
 
-	//enet_deinitialize();
+	/*enet_deinitialize();*/
 	CloseWindow();
 	return EXIT_SUCCESS;
 }
