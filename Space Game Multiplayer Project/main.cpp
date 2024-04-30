2f267e81da13ddcde3a001da5ac1249494bbf9ee
Dylan Garza <DAG.Gamer999@gmail.com>
2024-04-30 18:38:41 -0500

    DAGPacket implementation, using sstream to read received data

--- Space Game Multiplayer Project/main.cpp
+++ Space Game Multiplayer Project/main.cpp
@@ -5,6 +5,7 @@
 #include <list>
 #include <raylib.h>
 #include <raymath.h>
+#include <sstream>
 
 #define NOGDI
 #define CloseWindow CloseWinWindow
@@ -83,9 +84,9 @@ public:
 		DrawRectanglePro(display, center, RAD2DEG * angle, WHITE);
 
 		//debug displays
-		DrawRectangleLinesEx(hitbox, 2.0f, RED);
-		DrawCircle(hitbox.x, hitbox.y, 2.0f, RED);
-		DrawLine(display.x, display.y, display.x + 30 * cosf(angle), display.y + 30 * sinf(angle), RED);
+		//DrawRectangleLinesEx(hitbox, 2.0f, RED);
+		//DrawCircle(hitbox.x, hitbox.y, 2.0f, RED);
+		//DrawLine(display.x, display.y, display.x + 30 * cosf(angle), display.y + 30 * sinf(angle), RED);
 	}
 
 	void drawHUD(int x, int y) {
@@ -118,16 +119,39 @@ private:
 	float acceleration{};
 };
 
-//TODO:
-/*
-* kill benjamin
-* create network which can send and receive a ships's positional data (round data to 2 decimal precision?) 
-*/
+//ship controlled by a player
+class Ship {
+public:
+	Ship() {
+
+	}
+
+	void drawHUD(int x, int y) {
+		DrawText(TextFormat("a: %3.1f px/s^2", acceleration), 10 + x, 10 + y, 10, WHITE);
+		DrawText(TextFormat("v: %4.1f / %4.1f px/s", velocity, maxVelocity), 10 + x, 30 + y, 10, WHITE);
+		DrawText(TextFormat("x: %.1f, y: %.1f", component->get_hitbox().x, component->get_hitbox().x), 10 + x, 50 + y, 10, WHITE);
+		DrawText(TextFormat("angle: %.2f", angle), 10 + x, 70 + y, 10, WHITE);
+	}
+
+private:
+	Component* component{};
+
+	float angle{};
+	float velocity{};
+	float maxVelocity{}; //arbitrary
+	float acceleration{};
+	//std::list<Component> components;
+};
 
 enum class NetworkMode {
 	SINGLEPLAYER = 0,
 	CLIENT,
 	SERVER
+};
+
+enum GAME_STATES {
+	TITLE = 0,
+	GAME = 1
 };
 
 void initENet() {
@@ -147,33 +171,148 @@ NetworkMode promptNetMode() {
 	return (NetworkMode)nM;
 }
 
+class DAGPacket {
+public:
+	DAGPacket() {}
+
+	DAGPacket(ENetPacket* inPacket) {
+		const char* cData = (const char*)inPacket->data;
+		data = cData;
+
+		std::string line{};
+		std::stringstream reader(data);
+
+		while (reader >> line) {
+			dataArr.emplace_back(line);
+			line.clear();
+		}
+	}
+
+	template <typename Ty>
+	void append(Ty val) {
+		if (data.empty()) {
+			data.append(std::to_string(val));
+		}
+		else {
+			data.push_back(' ');
+			data.append(std::to_string(val));
+		}
+	}
+
+	void append(const char* txt) {
+		if (data.empty()) {
+			data.append(txt);
+		}
+		else {
+			data.push_back(' ');
+			data.append(txt);
+		}
+	}
+
+	ENetPacket* makePacket(enet_uint32 packetFlags) {
+		return enet_packet_create(data.c_str(), strlen(data.c_str()) + 1, packetFlags);
+	}
+
+	const std::string& get_data() {
+		return data;
+	}
+
+	const std::vector<std::string>& get_data_array() {
+		return dataArr;
+	}
+private:
+	std::string data{};
+	std::vector<std::string> dataArr{};
+};
+
+class Button {
+public:
+	Button(Rectangle rec, Color clr, bool enabled) {
+		hitbox = rec;
+		color = clr;
+		this->enabled = enabled;
+	}
+
+	bool clicked() {
+		return CheckCollisionPointRec(GetMousePosition(), hitbox) && IsMouseButtonPressed(MouseButton::MOUSE_BUTTON_LEFT) && enabled;
+	}
+
+	void enable() {
+		enabled = true;
+	}
+
+	void disable() {
+		enabled = false;
+	}
+
+	bool isEnabled() {
+		return enabled;
+	}
+
+	void draw() {
+		if (!enabled) return;
+		DrawRectangleRec(hitbox, color);
+	}
+
+private:
+	Rectangle hitbox{ 0, 0, 10, 10 };
+	Color color{ RED };
+	bool enabled{};
+};
+
 int main() {
 	initENet();
 
-	//initialize in console as server or client prior to game start
+	InitWindow(800, 800, "Space Game Multiplayer Project");
+	SetTargetFPS(60);
+
+	std::string input_str{};
 	NetworkMode netMode = NetworkMode::SINGLEPLAYER;
-	netMode = promptNetMode();
 
 	ENetHost* localHost{};
-	
+
 	ENetAddress localAddress{};
 	localAddress.host = ENET_HOST_ANY;
 	localAddress.port = 7777;
 
-	//init host by netMode
-	switch (netMode) {
-	case NetworkMode::SINGLEPLAYER: {
-		break;
+	int game_state = GAME_STATES::TITLE;
+	Button btn_singleplayer({ 10, 10, 50, 50 }, RED, true);
+	Button btn_client({ 70, 10, 50, 50 }, GREEN, true);
+	Button btn_server({ 130, 10, 50, 50 }, BLUE, true);
+	
+	//select net mode and init localHost
+	while (!WindowShouldClose()) {
+		if (btn_singleplayer.clicked()) {
+			netMode = NetworkMode::SINGLEPLAYER;
+			break;
+		}
+
+		if (btn_client.clicked()) {
+			netMode = NetworkMode::CLIENT;
+			localHost = enet_host_create(NULL, 1, 2, 0, 0);
+
+			break;
+		}
+
+		if (btn_server.clicked()) {
+			netMode = NetworkMode::SERVER;
+			localHost = enet_host_create(&localAddress, 1, 2, 0, 0);
+
+			break;
+		}
+
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		btn_singleplayer.draw();
+		btn_client.draw();
+		btn_server.draw();
+		
+		EndDrawing();
 	}
-	case NetworkMode::CLIENT: {
-		localHost = enet_host_create(NULL, 1, 2, 0, 0);
-		break;
-	}
-	case NetworkMode::SERVER: {
-		localHost = enet_host_create(&localAddress, 1, 2, 0, 0);
-		break;
-	}
-	}
+	btn_singleplayer.disable();
+	btn_client.disable();
+	btn_server.disable();
 
 	if (localHost == nullptr && netMode != NetworkMode::SINGLEPLAYER) {
 		fprintf(stderr, "Failed to initialize localHost for networking.\n");
@@ -187,14 +326,43 @@ int main() {
 	//connection handling
 	//TODO: make code return to input page if connection fails
 	ENetPeer* serverPeer{}; //external for later use
+	std::string addressStr{};
+	//
+	while (!WindowShouldClose() && netMode == NetworkMode::CLIENT) {
+		ENetAddress targetAddr{};
+		ENetEvent connectEvent{};
+
+		int key = GetKeyPressed();
+		char c = GetCharPressed();
+		std::cout << (int)netMode << "\n";
+		std::cout << addressStr << "\n";
+
+		if (c <= '9' && c >= '0' || c == '.') {
+			addressStr.push_back(c);
+		}
+
+		if (key == KeyboardKey::KEY_BACKSPACE) {
+			if (addressStr.size() != 0) addressStr.pop_back();
+		}
+
+		if (key == KeyboardKey::KEY_ENTER) break;
+
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		DrawText(addressStr.c_str(), 10, 10, 10, BLACK);
+
+		EndDrawing();
+	}
+	
 	if (netMode == NetworkMode::CLIENT) {
 		ENetAddress targetAddr{};
 		ENetEvent connectEvent{};
 
-		std::string addressStr{};
+		/*std::string addressStr{};
 		std::cout << "\nPlease enter an IPv4 address to connect to:\n\tAddress: ";
-		std::cin >> addressStr;
-
+		std::cin >> addressStr;*/
+		
 		enet_address_set_host(&targetAddr, addressStr.c_str());
 		targetAddr.port = 7777;
 
@@ -204,9 +372,9 @@ int main() {
 			exit(EXIT_FAILURE);
 		}
 
-		//try for 5 seconds to connect
+		//try for 30 seconds to connect
 		//exit if failure
-		if (enet_host_service(localHost, &connectEvent, 5000) > 0 && connectEvent.type == ENET_EVENT_TYPE_CONNECT) {
+		if (enet_host_service(localHost, &connectEvent, 30000) > 0 && connectEvent.type == ENET_EVENT_TYPE_CONNECT) {
 			printf("Successful connection to server!\n");
 		}
 		else {
@@ -219,9 +387,14 @@ int main() {
 	
 	//end client/server init
 
-	InitWindow(800, 800, "Space Game Multiplayer Project");
-	SetTargetFPS(60);
+	
 
+	//---------------------------------------------------------------------------//
+	//---------------------------------------------------------------------------//
+	// GAME CODE STARTS HERE
+	//---------------------------------------------------------------------------//
+	//---------------------------------------------------------------------------//
+	
 	//std::vector<Component*> players;
 	//players.emplace_back(480.0f);
 
@@ -230,84 +403,59 @@ int main() {
 
 	ENetEvent netEvent{};
 	while (!WindowShouldClose()) {
-		while (enet_host_service(localHost, &netEvent, 0)) {
-			switch (netEvent.type) {
-			case ENET_EVENT_TYPE_CONNECT: {
-				//players.emplace_back(480.0f);
-				if (netMode == NetworkMode::SERVER) {
-					ENetPacket* packet = enet_packet_create("Welcome to the server!", 23, ENET_PACKET_FLAG_RELIABLE);
-					enet_host_broadcast(localHost, 0, packet);
-				}
-				break;
-			}
-			case ENET_EVENT_TYPE_RECEIVE: {
-				std::cout << netEvent.packet->data << "\n";
-				const char* data = (const char*)netEvent.packet->data;
-				std::string str_data{ data };
-
-				float rX{};
-				float rY{};
-				float rA{};
-
-				auto it = str_data.begin();
+		if (netMode != NetworkMode::SINGLEPLAYER) {
+			while (enet_host_service(localHost, &netEvent, 0)) {
 				
-				std::string xStr{};
-				while (*it != ' ') {
-					xStr.push_back(*it);
-					it++;
+				switch (netEvent.type) {
+					case ENET_EVENT_TYPE_CONNECT: {
+						if (netMode == NetworkMode::SERVER) {
+							//ENetPacket* packet = enet_packet_create("Welcome to the server!", 23, ENET_PACKET_FLAG_RELIABLE);
+							DAGPacket packet;
+							packet.append("Welcome to the server!");
+						
+							enet_host_broadcast(localHost, 0, packet.makePacket(ENET_PACKET_FLAG_RELIABLE));
+						}
+						break;
+					}
+					case ENET_EVENT_TYPE_RECEIVE: {
+						DAGPacket rPacket(netEvent.packet);
+						//std::cout << rPacket.get_data() << "\n";
+						std::vector<std::string> rData = rPacket.get_data_array();
+
+						float rX = std::atof(rData.at(0).c_str());
+						float rY = std::atof(rData.at(1).c_str());
+						float rA = std::atof(rData.at(2).c_str());
+
+						otherPlayer.set_hitbox({ rX, rY, otherPlayer.get_hitbox().width, otherPlayer.get_hitbox().height });
+						otherPlayer.set_angle(rA);
+
+						enet_packet_destroy(netEvent.packet);
+						break;
+					}
 				}
+			}
 
-				it++;
-				std::string yStr{};
-				while (*it != ' ') {
-					yStr.push_back(*it);
-					it++;
-				}
+			//get x and y of localHost's ship, prepare as packet to send to peers
+			float sX = c.get_hitbox().x;
+			float sY = c.get_hitbox().y;
+			float sAngle = c.get_angle();
 
-				it++;
-				std::string angleStr{};
-				while (it != str_data.end()) {
-					angleStr.push_back(*it);
-					it++;
-				}
+			DAGPacket sPacket;
+			sPacket.append(sX);
+			sPacket.append(sY);
+			sPacket.append(sAngle);
 
-				rX = std::atof(xStr.c_str());
-				rY = std::atof(yStr.c_str());
-				rA = std::atof(angleStr.c_str());
-				std::cout << "x set to " << rX << "\n";
-				std::cout << "y set to " << rY << "\n";
-				std::cout << "angle set to " << rA << "\n";
-
-				otherPlayer.set_hitbox({rX, rY, 32, 32});
-				otherPlayer.set_angle(rA);
-
-				enet_packet_destroy(netEvent.packet);
+			ENetPacket* packet = sPacket.makePacket(ENET_PACKET_FLAG_RELIABLE);
+			switch (netMode) {
+			case NetworkMode::CLIENT:
+				enet_peer_send(serverPeer, 0, packet);
+				break;
+			case NetworkMode::SERVER:
+				enet_host_broadcast(localHost, 0, packet);
 				break;
 			}
-			}
 		}
 		
-		//get x and y of localHost's ship, prepare as packet to send to peers
-		float sendX = c.get_hitbox().x;
-		float sendY = c.get_hitbox().y;
-		float sendAngle = c.get_angle();
-
-		std::string packet_str{};
-
-		packet_str.append(std::to_string(sendX));
-		packet_str.append(" ");
-		packet_str.append(std::to_string(sendY));
-		packet_str.append(" ");
-		packet_str.append(std::to_string(sendAngle));
-		
-
-		ENetPacket* packet = enet_packet_create(packet_str.c_str(), strlen(packet_str.c_str()) + 1, ENET_PACKET_FLAG_RELIABLE);
-		if (netMode == NetworkMode::CLIENT) {
-			enet_peer_send(serverPeer, 0, packet);
-		}
-		else if (netMode == NetworkMode::SERVER) {
-			enet_host_broadcast(localHost, 0, packet);
-		}
 
 		c.control();
 
@@ -317,13 +465,16 @@ int main() {
 		c.draw();
 		c.drawHUD(0, 0);
 
-		otherPlayer.draw();
-		otherPlayer.drawHUD(0, 600);
+		if (netMode != NetworkMode::SINGLEPLAYER) {
+			otherPlayer.draw();
+			otherPlayer.drawHUD(0, 600);
+		}
+		
 
 		EndDrawing();
 	}
 
-	//enet_deinitialize();
+	enet_deinitialize();        
 	CloseWindow();
 	return EXIT_SUCCESS;
 }
